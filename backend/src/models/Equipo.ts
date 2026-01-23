bef52668e4b174849de87a6fd0c0d915dc06179a
nicocalderon7 <nicolasarielcalderon@gmail.com>
2026-01-23 11:42:14 -0300

    Create model Equipo.ts

---
+++ backend/src/models/Equipo.ts
@@ -0,0 +1,34 @@
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../config/database.js';
+import Categoria from './Categoria.js';
+
+class Equipo extends Model {}
+
+Equipo.init({
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  id_categoria: { 
+    type: DataTypes.INTEGER, 
+    references: { model: Categoria, key: 'id' } 
+  },
+  nombre: { type: DataTypes.STRING, allowNull: false },
+  marca: { type: DataTypes.STRING },
+  modelo: { type: DataTypes.STRING },
+  nro_serie: { type: DataTypes.STRING },
+  estado_operativo: { type: DataTypes.ENUM('funcional', 'dañado', 'en_reparacion') }, // Ajustar a tu enum
+  nro_patrimonio: { type: DataTypes.STRING },
+  observaciones: { type: DataTypes.TEXT },
+  propietario: { type: DataTypes.TEXT },
+  codigo: { type: DataTypes.STRING },
+  patrimonio_unq: { type: DataTypes.BOOLEAN, defaultValue: false },
+  imagen_url: { type: DataTypes.STRING }
+}, {
+  sequelize,
+  tableName: 'equipos',
+  underscored: true,
+});
+
+// Relación según tu DER
+Categoria.hasMany(Equipo, { foreignKey: 'id_categoria' });
+Equipo.belongsTo(Categoria, { foreignKey: 'id_categoria' });
+
+export default Equipo;
