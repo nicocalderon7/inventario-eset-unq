4b78aaaec68a902b6a4da2eaafe59b3ca932c79d
nicocalderon7 <nicolasarielcalderon@gmail.com>
2026-01-24 11:26:24 -0300

    Update index.ts
    Updated with middlewares and endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,15 +2,23 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import sequelize from './config/database.js'; 
+
+// Modelos
 import './models/Usuario.js';
 import './models/Categoria.js';
 import './models/Prestamo.js';
 import './models/Equipo.js';
 import './models/Mantenimiento.js';
+
+// Rutas
 import categoriaRoutes from './routes/categoriaRoutes.js';
 import equipoRoutes from './routes/equipoRoutes.js';
 import usuarioRoutes from './routes/usuarioRoutes.js';
 import prestamoRoutes from './routes/prestamoRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+
+// Middleware 
+import { verificarToken } from './middlewares/authMiddleware.js';
 
 dotenv.config();
 
@@ -22,10 +30,14 @@ app.use(cors());
 app.use(express.json());
 
 // Rutas
-app.use('/api/categorias', categoriaRoutes);
-app.use('/api/equipos', equipoRoutes);
-app.use('/api/usuarios', usuarioRoutes);
-app.use('/api/prestamos', prestamoRoutes);
+// 1. Rutas PÚBLICAS (No requieren Token)
+app.use('/api/auth', authRoutes);
+app.use('/api/categorias', categoriaRoutes); // Los usuarios pueden ver qué tipos de equipos hay
+
+// 2. Rutas PROTEGIDAS (Requieren Token)
+app.use('/api/equipos', verificarToken, equipoRoutes);
+app.use('/api/usuarios', verificarToken, usuarioRoutes);
+app.use('/api/prestamos', verificarToken, prestamoRoutes);
 
 // Función para conectar a la base de datos
 const conectarDB = async () => {
