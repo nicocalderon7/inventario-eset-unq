9d3675d2b07b2467360cfbe76539bff3200a7764
nicocalderon7 <nicolasarielcalderon@gmail.com>
2026-01-25 21:12:09 -0300

    Update swagger.ts
    Update connection with Railway

--- backend/src/config/swagger.ts
+++ backend/src/config/swagger.ts
@@ -6,7 +6,19 @@ const options: swaggerJSDoc.Options = {
     info: {
       title: 'API de Inventario ESET-UNQ',
       version: '1.0.0',
+      description: 'Documentación de la API para la gestión de inventario',
     },
+    // Si no pones esto, el desplegable de "Servers" desaparece
+    servers: [
+      {
+        url: 'https://inventario-eset-unq-production.up.railway.app',
+        description: 'Servidor de Producción (Railway)',
+      },
+      {
+        url: 'http://localhost:3000',
+        description: 'Servidor Local',
+      },
+    ],
     components: {
       securitySchemes: {
         bearerAuth: {
@@ -17,9 +29,14 @@ const options: swaggerJSDoc.Options = {
       },
     },
   },
-  // Esto busca en la carpeta donde esté el proceso ejecutándose, 
-  // sin importar si es src o dist.
-  apis: ['./src/routes/*.ts','./dist/routes/*.js'], 
+  // Agregamos rutas más profundas para que encuentre los archivos sí o sí
+  apis: [
+    './src/routes/*.ts',
+    './dist/routes/*.js',
+    './src/routes/**/*.ts',
+    './dist/routes/**/*.js',
+    './routes/*.js'
+  ],
 };
 
 export const swaggerSpec = swaggerJSDoc(options);
